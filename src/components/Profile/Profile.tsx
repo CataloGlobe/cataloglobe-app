24bb92ba94d56abc334ae4ae9226eb97a217a55f
Lorenzo Calzi <lorenzo.calzi@primoround.com>
2026-01-26 00:15:38 +0100

    corretto type del button

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -102,6 +102,7 @@ export default function Profile() {
                         />
 
                         <Button
+                            type="submit"
                             variant="primary"
                             fullWidth
                             loading={loading}
